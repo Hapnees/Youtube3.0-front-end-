76b3b127c2e0d511f6712829daadce238ac1d96f
Hapnees <8dwardm8lton@gmail.com>
2022-10-19 04:13:03 +0300

    Feat (Loader): new loader

--- src/components/ui/LoaderUI/Loader.tsx
+++ src/components/ui/LoaderUI/Loader.tsx
@@ -3,9 +3,15 @@ import cl from './Loader.module.scss'
 
 const Loader = () => {
 	return (
-		<div className='w-full mt-[100px]'>
-			<p className={cl.title}>Загрузка...</p>
-			<div className={cl.loader}></div>
+		<div className='w-full mt-[100px] flex justify-center'>
+			<div className='flex flex-col items-center gap-12'>
+				<p className={cl.title}>Загрузка...</p>
+				<div className={cl.loader}>
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+			</div>
 		</div>
 	)
 }
